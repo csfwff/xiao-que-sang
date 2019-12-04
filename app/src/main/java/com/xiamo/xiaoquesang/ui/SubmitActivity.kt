6aa2599fccd2978f4aa208c76d83018b01751544
夏墨 <csfwff@163.com>
2019-12-04 16:52:55 +0800

    完成投稿页面布局

---
+++ app/src/main/java/com/xiamo/xiaoquesang/ui/SubmitActivity.kt
@@ -0,0 +1,17 @@
+package com.xiamo.xiaoquesang.ui
+
+
+import com.xiamo.xiaoquesang.R
+import com.xiamo.xiaoquesang.common.BaseActivity
+
+class SubmitActivity : BaseActivity() {
+    override fun getLayoutId(): Int {
+        return R.layout.activity_submit
+    }
+
+    override fun initView() {
+
+    }
+
+
+}
