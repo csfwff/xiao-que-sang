c43252cc6b43f7ce73fa80f2bfc57bde02dba311
夏墨 <csfwff@163.com>
2019-12-12 15:58:24 +0800

    完成个人信息

--- app/src/main/java/com/xiamo/xiaoquesang/common/widget/TitleBar.kt
+++ app/src/main/java/com/xiamo/xiaoquesang/common/widget/TitleBar.kt
@@ -1,14 +1,18 @@
 package com.xiamo.xiaoquesang.common.widget
 
+import android.app.Activity
 import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
+import android.view.View
 import android.widget.RelativeLayout
 import com.xiamo.xiaoquesang.R
+import kotlinx.android.synthetic.main.widget_title_bar.view.*
 
 class TitleBar:RelativeLayout{
 
     private var mOnBackClickListener :OnBackClickListener?=null
+    private var mOnRightClickListener :OnRightClickListener?=null
 
     @JvmOverloads constructor(
         context: Context,
@@ -16,7 +20,35 @@ class TitleBar:RelativeLayout{
         defStyleAttr:Int = 0
     ):super(context, attrs,defStyleAttr){
         attrs.let{
+            val typedArray = context.obtainStyledAttributes(it,R.styleable.TitleBar)
+            val title = typedArray.getString(R.styleable.TitleBar_title)
+            val isBackShow = typedArray.getBoolean(R.styleable.TitleBar_isBackShow,true)
+            val isRightShow = typedArray.getBoolean(R.styleable.TitleBar_isRightShow,false)
+            val backSrc = typedArray.getDrawable(R.styleable.TitleBar_backImg)
+            val rightSrc = typedArray.getDrawable(R.styleable.TitleBar_rightImg)
+            typedArray.recycle()
 
+            titleTv.text = title
+            backImg.visibility = if (isBackShow) View.VISIBLE else View.INVISIBLE
+            rightImg.visibility = if (isRightShow) View.VISIBLE else View.INVISIBLE
+
+            backImg.setImageDrawable(backSrc)
+            rightImg.setImageDrawable(rightSrc)
+
+
+            backImg.setOnClickListener {
+                if(mOnBackClickListener==null){
+                    (context as Activity).finish()
+                }else{
+                    mOnBackClickListener!!.onBackClick()
+                }
+            }
+
+            rightImg.setOnClickListener {
+                if(mOnRightClickListener!=null){
+                    mOnRightClickListener!!.onRightClick()
+                }
+            }
         }
     }
 
@@ -30,5 +62,13 @@ class TitleBar:RelativeLayout{
 
     interface OnBackClickListener{
         fun onBackClick()
+    }
+
+    fun setOnRightClickListener( listener: OnRightClickListener){
+        if(mOnRightClickListener==null) mOnRightClickListener = listener
+    }
+
+    interface OnRightClickListener{
+        fun onRightClick()
     }
 }
